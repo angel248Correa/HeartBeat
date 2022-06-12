8feab07da621829c8ffd4a6c038000007f65ae51
AngelC <angelcorrea248@gmail.com>
2022-06-12 12:47:57 -0400

    Initial commit

---
+++ src/heartbeat/HeartModel.java
@@ -0,0 +1,15 @@
+package heartbeat;
+
+public class HeartModel implements Runnable {
+
+	 public void getHeartRate() { 
+		 
+	 }
+	 public void registerBeatObserver() { 
+		 
+	 }
+	 public void registerBPMObserver() { 
+		 
+	 }
+	  
+}
